a47a42d201d66ca446725b9bb81a9cd6d223ea6d
Javier Angosto <javier.angosto@gmail.com>
2023-03-22 22:23:37 +0100

    new Router for Hero

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,20 +1,15 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 
 import { LoginPage } from "../auth"
-import { MarvelPage, DCPage } from "../heroes"
-import { Navbar } from "../ui"
+import { HeroesRoutes } from "../heroes"
 
 
 export const AppRouter = () => {
     return (
     <>
-        <Navbar />
-
         <Routes>
-            <Route path = "marvel" element = {<MarvelPage />} />
-            <Route path = "dc" element = {<DCPage />} />
-            <Route path = "login" element = {<LoginPage />} />
-            <Route path = "/" element = {<Navigate to = "/marvel" />} />
+            <Route path = "login" element = { <LoginPage /> } />
+            <Route path = "/*" element = { <HeroesRoutes /> } />
         </Routes>
     </>
 
