e20be76c7803e3657c5ba12686880dc280a5147d
Javier Angosto <javier.angosto@gmail.com>
2023-03-22 22:33:32 +0100

    useNavigate on Logout/Login

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,17 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 export const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+
+        navigate( '/login', {
+            replace: true,
+        });
+
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
@@ -37,6 +48,7 @@ export const Navbar = () => {
                     </span>
                     <button
                         className='nav-item nav-link btn btn-primary'
+                        onClick={ handleLogout }
                     >
                         Logout
                     </button>
