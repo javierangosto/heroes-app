e58bc0adb8514e062ed783b9735c1416cc236a4c
Javier Angosto <javier.angosto@gmail.com>
2023-03-23 11:29:19 +0100

    Search Page form working

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,31 @@
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
+import queryString from 'query-string';
+
+import { useForm } from "../../hooks/useForm";
 import { HeroCard } from "../components";
 
 export const SearchPage = () => {
+
+    const navigate = useNavigate();
+    const location = useLocation(); //Location devuelve un objeto con la ruta actual, parametros, ...
+
+    const { q = '' } = queryString.parse(location.search); //querystring devuelve un objeto con los parametros separado
+
+    const { searchText, handleInputChange } = useForm({
+        searchText: '',
+    });
+
+    const handleSarchSubmit = ( event ) => {
+        event.preventDefault();
+
+        if ( searchText.trim() <= 1 ) return;
+
+        //si no se indica ruta en el navigate, por defecto navegará a la página actual
+        navigate(`?q=${ searchText }`);
+
+
+    }
+
     return (
         <>
 
@@ -11,13 +36,15 @@ export const SearchPage = () => {
                 <div className="col-5">
                     <h4>Searching</h4>
                     <hr/>
-                    <form>
+                    <form onSubmit={ handleSarchSubmit }>
                         <input
                             type="text"
                             placeholder="Search a hero..."
                             className="form-control"
                             name="searchText"
                             autoComplete="off"
+                            value = { searchText }
+                            onChange = { handleInputChange }
                         / >
                         <button
                             className="btn btn-outline-primary mt-2"    
@@ -35,7 +62,7 @@ export const SearchPage = () => {
                         Search a hero
                     </div>
                     <div className="alert alert-danger">
-                        No hero with <b>ABC</b>
+                        No hero with <b>{ q }</b>
                     </div>
                     
                     {/* <HeroCard /> */}
