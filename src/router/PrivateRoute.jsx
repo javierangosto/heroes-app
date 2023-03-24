56344a052e0ec56f95f0ea371cbf333030d2f793
Javier Angosto <javier.angosto@gmail.com>
2023-03-24 11:44:17 +0100

    lastPath visited done & move img to public

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -6,6 +6,11 @@ export const PrivateRoute = ( { children }) => {
 
     const { logged } = useContext( AuthContext );
 
+    const { pathname, search } = useLocation();
+
+    const lastPath = pathname + search;
+    localStorage.setItem( 'lastPath', lastPath );
+
     return ( logged )
     ? children
     : <Navigate to = '/login' />
